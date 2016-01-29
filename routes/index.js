b3da22cc97781c0f8d1856d944a8ad944246f41c
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-01-29 12:39:51 -0600

    created working link for 'liked dogs'

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET liked page. */
+router.get('/liked', function(req, res, next) {
+  res.render('liked', { title: 'Fave Dogs: ' });
+});
+
 module.exports = router;
