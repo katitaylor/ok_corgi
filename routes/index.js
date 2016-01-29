113ae25d968791025803be02dadb8c9c22b4d69f
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-01-29 15:14:20 -0600

    left paw link works

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var Dog = require('../models/dog');
+
+/* paw left */
+router.get('/left', function(req, res, next) {
+  res.render('left', {title: 'Paw Left'});
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
